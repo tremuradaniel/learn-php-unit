f0aa928e34f9b76c0ccd7f2dc967d26ce96fafdf
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-30 08:22:17 +0200

    config phpunit and first test

---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+    use PHPUnit\Framework\TestCase;
+
+    class SampleTest extends TestCase {
+        public function testTrueAssertsToTrue() {
+            $this->assertTrue(false);
+        }
+    }
